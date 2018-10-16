dfa8ff584fd47f80259df7b5131bd3aa06524f61
Ismael Nascimento <contato@ismaelnascimento.com>
2018-10-15 21:00:28 -0300

    Initial commit

---
+++ Assets/_Project/Scripts/MyTrackableEventHandler.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using Vuforia;
+
+public class MyTrackableEventHandler : MonoBehaviour, ITrackableEventHandler
+{
+    [SerializeField] private TrackableBehaviour trackableBehaviour;
+    [SerializeField] private GameObject canvasControllers;
+
+    private void Start()
+    {
+        trackableBehaviour.RegisterTrackableEventHandler(this);
+    }
+
+    public void OnTrackableStateChanged(TrackableBehaviour.Status previousStatus, TrackableBehaviour.Status newStatus)
+    {
+        if (newStatus == TrackableBehaviour.Status.DETECTED ||
+            newStatus == TrackableBehaviour.Status.TRACKED ||
+            newStatus == TrackableBehaviour.Status.EXTENDED_TRACKED)
+        {
+            canvasControllers.SetActive(true);
+        }
+        else if (previousStatus == TrackableBehaviour.Status.TRACKED &&
+                 newStatus == TrackableBehaviour.Status.NOT_FOUND)
+        {
+            canvasControllers.SetActive(false);
+        }
+        else
+        {
+            canvasControllers.SetActive(false);
+        }
+    }
+}
---
+++ Assets/_Project/Scripts/VirtualButtonManager.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using Vuforia;
+using UnityEngine.Video;
+
+public class VirtualButtonManager : MonoBehaviour, IVirtualButtonEventHandler
+{
+    [SerializeField] private UnityEngine.UI.Image imagePanel;
+    [SerializeField] private VideoPlayer videoPlayer;
+
+    void Start()
+    {
+        imagePanel.color = Color.green;
+        GetComponent<VirtualButtonBehaviour>().RegisterEventHandler(this);
+    }
+
+    public void OnButtonPressed(VirtualButtonBehaviour vb) // Sobre botão virtual
+    {
+        WhenIsPress();
+    }
+
+    public void OnButtonReleased(VirtualButtonBehaviour vb) // Retira botão virtual
+    {
+        WhenIsReleased();
+    }
+
+    private void WhenIsPress()
+    {
+        imagePanel.color = Color.blue;
+        PlayOrPauseVideo();
+        print("WhenIsPress");
+    }
+
+    private void WhenIsReleased()
+    {
+        imagePanel.color = Color.green;
+        print("WhenIsReleased");
+    }
+
+    private void PlayOrPauseVideo()
+    {
+        if (!videoPlayer.isPlaying)
+            videoPlayer.Play();
+        else
+            videoPlayer.Pause();
+    }
+}
